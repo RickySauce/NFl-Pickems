d484294afb186161cd46f38de30d3c8ce11b1044
learn <eric.desimone@cortland.edu>
2018-10-13 07:01:49 -0700

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many: :user_leagues
-  has_many: :leagues, through: :user_leagues
+  has_many :user_leagues
+  has_many :leagues, through: :user_leagues
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true
