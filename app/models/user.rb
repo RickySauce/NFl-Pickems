a7e2a4ebf319d23157fa4577ecfbfc2c9d818ce3
learn <eric.desimone@cortland.edu>
2018-10-13 06:59:43 -0700

    added validations to User, has_secure_password and has_many belongs to connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many: :user_leagues
+  has_many: :leagues, through: :user_leagues
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
