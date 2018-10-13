bde051e70c1111fb6adde5835784e8c01425634f
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-13 08:01:47 -0700

    home page

---
+++ client/src/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+const Header = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1 className="App-title">NFL PICKEMS</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
