5f7c680b25fcaa9e1d12b59d95a9109002db45fc
learn <eric.desimone@cortland.edu>
2018-10-13 07:23:21 -0700

    added belongs_to associations for team and league

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,5 @@
 class League < ApplicationRecord
   has_many :user_leagues
   has_many :leagues, through: :user_leagues
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
 end
--- app/models/matchup.rb
+++ app/models/matchup.rb
@@ -1,4 +1,6 @@
 class Matchup < ApplicationRecord
   belongs_to :week
   has_many :user_picks
+  belongs_to :home_team, :class_name => 'Team', :foreign_key => 'home_id'
+  belongs_to :away_team, :class_name => 'Team', :foreign_key => 'away_id'
 end
