2df4f759273172b7fc3a3c890f2f225ccbfaf8b5
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-13 06:53:32 -0700

    login backend

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,15 @@
 class SessionController < ApplicationController
+
+
+  def new
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+    render json: @user, status: 200
+    else
+    render json: {message: "Invalid Username Or Password"}, status: 406
+    end
+  end
+
+
+
 end
