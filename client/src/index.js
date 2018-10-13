ac0ab0e4d4890d8f292a352763ad2b07ad5cabf5
learn <eric.desimone@cortland.edu>
2018-10-13 07:35:42 -0700

    setup index component with redux, middleware, thunk, react-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,26 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/rootReducer';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <React.Fragment>
+        <Route path="/" component={App}/>
+      </React.Fragment>
+    </Router>
+  </Provider>,
+document.getElementById('root')
+)
+registerServiceWorker();
