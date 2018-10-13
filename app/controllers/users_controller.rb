9fc36b730e345beb2ad94fb9bcb0a4dedec076c4
learn <eric.desimone@cortland.edu>
2018-10-13 07:32:38 -0700

    added user signup functionality in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  def create
+  @user = User.new(user_params)
+  if @user.save
+    render :json => @user, status: 201
+ else
+   render :json => { :errors => @user.errors}
+ end
+end
+
+def show
+  @user = User.find_by(id: params[:id])
+  render :json => @user
+end
+
+
+
+private
+
+def user_params
+   params.require("user").permit(
+     :username,
+     :email,
+     :password,
+     :password_confirmation
+   )
+ end
+
 end
