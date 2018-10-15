1188850fd3ee467ed386a9a9751866daceb6552b
learn <eric.desimone@cortland.edu>
2018-10-15 09:35:50 -0700

    debugged fetchUser dispatch action

--- client/src/actions/fetchUser.js
+++ client/src/actions/fetchUser.js
@@ -1,8 +1,8 @@
 export function fetchUser(id){
+  console.log(id)
   return (dispatch) => {
     fetch(`/api/users/${id}`)
     .then(resp => resp.json())
     .then(json => dispatch({ type: 'LOG_IN', json}))
-
   };
 };
