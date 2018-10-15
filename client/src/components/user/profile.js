b52afdc66ad589c478f42f93bf387c1730ab2ab8
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-15 15:01:23 -0700

    panels and buttons

--- client/src/components/user/profile.js
+++ client/src/components/user/profile.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Panel, Button } from 'react-bootstrap'
 
 
 
 
   class Profile extends Component {
+
+    handleClick = event => {
+      console.log(event.target.value)
+    }
     render() {
       return (
-        <h1>Welcome {this.props.user.username}!</h1>
+        <div>
+        <h1 className="welcome">Welcome {this.props.user.username}!</h1>
+        <Panel>
+        <Panel.Heading>My Leagues</Panel.Heading>
+        <Panel.Body >League 1 name <Button bsStyle="info" bsSize="xsmall" value={1} onClick={this.handleClick}>CLICK ME</Button></Panel.Body>
+        <Panel.Body onClick={this.handleClick}>League 2 name</Panel.Body>
+        </Panel>
+        </div>
       )
     }
   }
