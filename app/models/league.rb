bedade984110f699c6e8aceca7d7a0433e4ff573
learn <eric.desimone@cortland.edu>
2018-10-16 04:37:29 -0700

    fixed model relations for league

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,5 @@
 class League < ApplicationRecord
   has_many :user_leagues
-  has_many :leagues, through: :user_leagues
+  has_many :users, through: :user_leagues
   belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
 end
