51ea5068e9306a9d0a25424264721a72dc33c305
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-16 14:07:02 -0700

    commit

--- client/src/components/leagues/createleague.js
+++ client/src/components/leagues/createleague.js
@@ -7,9 +7,15 @@ export default class LeagueForm extends Component {
 
   state = {
     count: 0,
-    inputs: []
+    inputs: [],
+    name: "",
+    players: []
   }
 
+  handleChange = (event) => {
+    this.setState({players: [...this.state.players, event.target.name: event.target.value]})
+    console.log(this.state)
+  }
 
   handleClick = (event) => {
     let input = `<input type="text" />`
@@ -19,13 +25,18 @@ export default class LeagueForm extends Component {
         return this.setState({count: this.state.count - 1, inputs: [...this.state.inputs.slice(0,-1)]})
       } else {return this.setState({count: 0})}
       case "increase":
-        return this.setState({count: this.state.count + 1, inputs: [...this.state.inputs, <input style={{display: "block"}}type='text' placeholder='Player Name'/>]})
+        return this.setState({count: this.state.count + 1, inputs: [...this.state.inputs, <input onChange={this.handleChange} name={"Player" + (this.state.count + 1)} style={{display: "block"}}type='text' placeholder='Player Name'/>]})
     }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log()
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
       <strong>Name of League</strong><br/><br/>
       <input type="text" name="name"/><br/><br/>
       <strong>Number of Players</strong>  <Button onClick={this.handleClick} name="decrease" bsSize="xsmall">-</Button><span>{this.state.count}</span><Button onClick={this.handleClick} name="increase" bsSize="xsmall">+</Button><br/><br/>
