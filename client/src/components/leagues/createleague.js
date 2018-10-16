aef9485eeb3bc77560d0290abcbcd67e5d963f3a
learn <eric.desimone@cortland.edu>
2018-10-16 08:06:43 -0700

    handle click for league create is functional

--- client/src/components/leagues/createleague.js
+++ client/src/components/leagues/createleague.js
@@ -16,7 +16,7 @@ export default class LeagueForm extends Component {
     switch (event.target.name) {
       case "decrease":
         if (this.state.count > 0) {
-        return this.setState({count: this.state.count - 1, inputs: [this.state.inputs.splice(-1,1)]})
+        return this.setState({count: this.state.count - 1, inputs: [...this.state.inputs.slice(0,-1)]})
       } else {return this.setState({count: 0})}
       case "increase":
         return this.setState({count: this.state.count + 1, inputs: [...this.state.inputs, <input style={{display: "block"}}type='text' placeholder='Player Name'/>]})
