3e53519fb2800e7ad45c22b9f9f750f2c8a50958
learn <eric.desimone@cortland.edu>
2018-10-20 13:27:09 -0400

    set up matchup, season and week seraializers

--- app/serializers/matchup_serializer.rb
+++ app/serializers/matchup_serializer.rb
@@ -1,6 +1,17 @@
 class MatchupSerializer < ActiveModel::Serializer
-  attributes :id
-
+  attributes :id, :winning_id, :game_date_time
+  belongs_to :home_team, :away_team
 
 end
-# .iso8601 for time conversion 
+# .iso8601 for time conversion
+
+
+create_table "matchups", force: :cascade do |t|
+  t.integer "week_id"
+  t.integer "home_id"
+  t.integer "away_id"
+  t.integer "winning_id"
+  t.datetime "game_date_time"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
--- app/serializers/week_serializer.rb
+++ app/serializers/week_serializer.rb
@@ -1,3 +1,4 @@
 class WeekSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :start_date_time, :end_date_time, :locked, :week_number
+  has_many :matchups
 end
