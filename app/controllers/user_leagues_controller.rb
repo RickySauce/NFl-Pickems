8c6b8868d618a562d86e438514c73b70ccdca0fb
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-22 15:11:27 -0700

    team logos and user remove working

--- app/controllers/user_leagues_controller.rb
+++ app/controllers/user_leagues_controller.rb
@@ -2,6 +2,7 @@ class UserLeaguesController < ApplicationController
 
   def destroy
     @user_league = UserLeague.find_user_league(params["league_id"],params["user_id"])
+    @user_league.destroy 
   end
 
   def create
