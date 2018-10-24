bcca2d06670fe09bab8bbfd91de7f6089db2a8b2
learn <eric.desimone@cortland.edu>
2018-10-24 10:29:48 -0400

    outlines team name onClick

--- client/src/components/leagues/seasons/weeks/matchups/team.js
+++ client/src/components/leagues/seasons/weeks/matchups/team.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Team = (props) => {
 
+const style = props.side.picked === true ? {border: '1px solid black'} : null
 
   return(
-    <span onClick={props.handleClick} data-side={props.side.side}>
+    <span onClick={props.handleClick} data-side={props.side.side} data-id={props.team.id} style={style}>
     {props.team.name}
     </span>
   )
