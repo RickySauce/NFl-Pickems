dd9e46cbd8f4a455e3a529d44ff2adb770c67737
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-25 11:21:14 -0700

    date of games

--- client/src/components/leagues/seasons/weeks/matchups/matchupCard.js
+++ client/src/components/leagues/seasons/weeks/matchups/matchupCard.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Team from './team'
-
+import moment from 'moment'
 class MatchupCard extends Component {
 
 state = {
   home: {side: "home", picked: false},
   away: {side: "away", picked: false}
+}
+
+dateFormatter = (date) => {
+  return moment(date).format('MM/DD/YYYY h:mma')
 }
 
 handleClick = (event) => {
@@ -19,11 +23,12 @@ handleClick = (event) => {
 }
 
   render(){
+    {console.log(this.props.matchup)}
     return(
 
       <ul>
       <Team team={this.props.matchup.homeTeam} side={this.state.home} handleClick={this.handleClick}/>   vs.  <Team team={this.props.matchup.awayTeam} side={this.state.away} handleClick={this.handleClick}/> <br/><br/>
-      <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{this.props.matchup.homeTeam.name}(Home)  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{this.props.matchup.awayTeam.name}(Away)</span>
+      <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{this.props.matchup.homeTeam.name}(Home)  &nbsp;<strong>{this.dateFormatter(this.props.matchup.gameDateTime)}</strong>&nbsp;{this.props.matchup.awayTeam.name}(Away)</span>
       </ul>
     )
   }
