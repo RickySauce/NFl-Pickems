f5de911f9a15f8427a57ad4bc5ba1d383ba4c059
mgoldfi1 <mgoldfi1@gmail.com>
2018-10-25 20:15:45 -0700

    timer first model working

--- client/src/components/leagues/seasons/weeks/matchups/timer.js
+++ client/src/components/leagues/seasons/weeks/matchups/timer.js
@@ -7,18 +7,29 @@ export default class Timer extends Component {
     super(props);
     this.state = {
       time: Date.parse(new Date()),
-      gameTime: Date.parse(this.props.time)
+      gameTime: Date.parse(this.props.time),
+      difference: 0
     }
   }
 
 
   changeTime = () => {
-    this.setState({time: this.state.time - 1000})
+    this.setState({difference: this.state.gameTime - Date.parse(new Date())})
   }
 
+  countDown = duration => {
+  var seconds = parseInt((duration / 1000) % 60),
+     minutes = parseInt((duration / (1000 * 60)) % 60),
+     hours = parseInt((duration / (1000 * 60 * 60)) % 24),
+	   days = parseInt((duration / (1000 * 60 * 60 * 24)));
+     hours = (hours < 10) ? "0" + hours : hours;
+     minutes = (minutes < 10) ? "0" + minutes : minutes;
+     seconds = (seconds < 10) ? "0" + seconds : seconds;
+     days = ( days < 10 ) ? "0" + days : days;
+  return days + " DAYS " + hours + " HOURS " + minutes + " MINUTES " + seconds + " SECONDS UNTIL START"
+}
+
   componentDidMount() {
-    setInterval(
-    this.changeTime, 1000)
     this.interval = setInterval(this.changeTime, 1000)
   }
 
@@ -28,7 +39,7 @@ export default class Timer extends Component {
 
   render () {
     return (
-      <div>{moment.duration(moment(new Date(this.state.gameTime), 'YYYY/MM/DD HH:mm').diff(moment(new Date(this.state.time), 'YYYY/MM/DD HH:mm'))).asHours()}</div>
+      <div style={{'font-size': '8px'}}>{(this.state.gameTime - this.state.time) < 0 ? "EXPIRED" : this.countDown(this.state.difference)}</div>
 
     )
   }
